cde9bfc8fe3869833966b7596d30bd04ba990c9d
katerine <chen.katerine@gmail.com>
2017-10-06 22:44:13 +0800

    modify 1

--- MyBeautySalon/app/src/main/java/com/example/user/mybeautysalon/MainActivity.java
+++ MyBeautySalon/app/src/main/java/com/example/user/mybeautysalon/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.user.mybeautysalon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks {
+    private SQLiteDatabase mDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getLoaderManager().initLoader(Loader_ID,null,this);
+    }
+
+    public void clickChk(View v){
+        Intent it=new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(it);
+    }
+
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
+        SQLiteOpenHelper dbHelper=new MyDBHelper(this);
+        mDB=dbHelper.getWritableDatabase();
+        MyCursorLoader loader=new MyCursorLoader(this,mDB);
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader loader, Object o) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
     }
 }
